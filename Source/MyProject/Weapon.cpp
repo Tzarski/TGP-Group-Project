13046c27a2950ecf4b40d8e4c1806de26950462f
Laurentiu Vasilachi <v022748h@student.staffs.ac.uk>
2019-02-26 19:53:10 +0000

    started creating the classes

---
+++ Source/MyProject/Weapon.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Weapon.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+AWeapon::AWeapon(const FObjectInitializer& PCIP) : Super(PCIP)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	WeaponComponent = PCIP.CreateDefaultSubobject<UPaperSpriteComponent>(this, TEXT("weapon"));
+	WeaponComponent->SetupAttachement(RootComponent);
+	WeaponComponent->SetSprite(ConstructorHelpers::FObjectFinder<UPaperSprite>(TEXT("PaperSprite'/Game/'")).Object)
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
